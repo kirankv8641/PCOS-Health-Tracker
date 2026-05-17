8734da35af17b7dc17aa43692b3409e02bb5780c
kirankv8641 <kv878432@gmail.com>
2026-05-17 23:43:55 +0530

    last  change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,9 +12,6 @@ import About       from "./pages/About";
 import AuthFlow    from "./pages/AuthFlow";
 import Profile     from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
-import WellnessScore from "./components/WellnessScore";
-
-
 
 
 import { initNotifications } from "./services/Notification";
@@ -43,7 +40,6 @@ function App() {
         <Route path="/login"       element={<AuthFlow />}    />
         <Route path="/profile"     element={<Profile />}     />
         <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/Wellness" element={<WellnessScore />} />
       </Routes>
     </Router>
   );
